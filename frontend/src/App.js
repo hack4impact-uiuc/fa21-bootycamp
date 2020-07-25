9ee4310277192fb38e33e4058295cb9f061398e4
Jacqueline <jackieo5023@gmail.com>
2020-07-25 18:52:42 -0500

    Add react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Link } from '@hack4impact-uiuc/bridge';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Link } from "@hack4impact-uiuc/bridge";
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </Link>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Link
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </Link>
+        </header>
+      </div>
+    </Router>
   );
 }
 
