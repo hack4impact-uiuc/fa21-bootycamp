0468f5ae655a46cbd9b23429d9d0bca11a4ff764
Jacqueline <jackieo5023@gmail.com>
2020-07-25 18:42:33 -0500

    Add bridge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from '@hack4impact-uiuc/bridge';
 
 function App() {
   return (
@@ -10,14 +11,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <Link
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </Link>
       </header>
     </div>
   );
