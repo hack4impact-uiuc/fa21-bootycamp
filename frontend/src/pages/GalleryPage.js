4937dcc2e0613d64306e1acd0eebdc19e7800c79
Brandon Wang <sourbrandon@gmail.com>
2020-10-03 15:37:28 -0500

    add an example card

--- frontend/src/pages/GalleryPage.js
+++ frontend/src/pages/GalleryPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import data from "../data/mockData";
+import { Card } from 'semantic-ui-react'
 
 import "../styles/GalleryPage.css";
 
@@ -10,6 +11,13 @@ export default function GalleryPage() {
       <h1>Gallery</h1>
       {/* This is the page that displays all the items in your database. For now, 
       hard-code these objects in the ../data/mockData.js file. */}
+
+      <Card
+        image='https://raw.githubusercontent.com/hack4impact-uiuc/Academy-Git-Demo/master/adorable.JPG?token=ADT32ZKMXF4TLRDLNN3JA627PDQSU'
+        header='Otter'
+        description='otter otter alma otter'
+      />
+
     </div>
   );
 }
