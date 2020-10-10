1a01142f97b7935d1a60bffcfce8b53545bef830
neha konjeti <nehakonjeti@nehas-mbp.lan>
2020-10-09 20:58:53 -0500

    added text to front page

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -14,3 +14,4 @@ export default function NavBar(props) {
     </div>
   );
 }
+
--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { Header, Image } from 'semantic-ui-react'
 
 import "../styles/LandingPage.css";
 
 export default function LandingPage() {
   return (
+   
     <div>
-       <h1 class = 'titles'>Welcome to my Restaurant!</h1>
+       <h1 class = 'titles'>Welcome to my Cat Cafe!</h1>
       {/* This is the homepage of your app. You should have a jumbotron, a button 
       leading to GalleryPage and another button leading to CreateObjectPage. */}
-      <div class="container">
-      <img src="https://retaildesignblog.net/wp-content/uploads/2014/04/Peppes-Pizza-restaurant-by-RISS-INTERIORARKITEKTER-Oslo-Norway.jpg" ></img>
+      <div class = 'container'>
+  <img class = "ui fluid image" src="https://retaildesignblog.net/wp-content/uploads/2014/04/Peppes-Pizza-restaurant-by-RISS-INTERIORARKITEKTER-Oslo-Norway.jpg"></img>
+  
+  
+      
       <button class = "massive ui button"><Link to="/gallery">Go to Gallery</Link></button>
       <div class = 'buttonclass'>
       <button class = "massive ui button" ><Link to="/create">Create user</Link></button>
+      <div class = 'info'>
+      <div class="ui medium header">Get served by Pusheen herself and our cat staff! Go to gallery to view our menu. </div>
+      </div>
+      </div>
       </div>
      
        </div>
@@ -26,6 +34,6 @@ export default function LandingPage() {
       
       
      
-    </div>
+
   );
 }
