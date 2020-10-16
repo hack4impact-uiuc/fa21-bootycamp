04cc3e43dd2d0d9640140b4e4cd4552177a9c4c7
Jain-inc <adityajain1921@gmail.com>
2020-10-15 22:36:47 -0400

    fixed card header issue

--- frontend/src/pages/GalleryPage.js
+++ frontend/src/pages/GalleryPage.js
@@ -19,104 +19,136 @@ import trap_hole from './images/trap_hole.jpg';
 
 export default function GalleryPage() {
   return (
-    <div class="cards">
-      <h1>Gallery</h1>
-      {/* This is the page that displays all the items in your database. For now, 
-      hard-code these objects in the ../data/mockData.js file. */}
+    <div>
+      <div id="title">
+        <h1>My Card Collection</h1>
+        {/* This is the page that displays all the items in your database. For now, 
+        hard-code these objects in the ../data/mockData.js file. */}
+      </div>
+    
+    <div id="cards">
+      <Card.Group itemsPerRow={4}>
+          
+          {/*<Card
+            image='http://cdn.pastemagazine.com/www/articles/assets_c/2019/06/TheOfficeQuote1-thumb-633x351-1115812.png'
+            header='Michael Scott'
+            description='this is michael'
+          />*/}
 
-    <Card.Group itemsPerRow={4}>
+          <Card
+            className = "monster"
+            as={Link} to ='/detail'
+            image={blueeyeswhitedragon}
+            href='Land'
+            header='Blue-Eyes White Dragon'
+            description='Monster Card'
+            fluid color='brown'
+          />
+          <Card
+            className = "monster"
+            as={Link} to ='/detail'
+            image={dark_magician}
+            header='Dark Magician'
+            description='Monster Card'
+            fluid color='brown'
+          />
+          <Card
+            className = "monster"
+            as={Link} to ='/detail'
+            image={exodia_the_forbidden_one}
+            header='Exodia the Forbidden One'
+            description='Monster Card'
+            fluid color='brown'
+          />
+          <Card
+            className = "monster"
+            as={Link} to ='/detail'
+            image={the_winged_dragon_of_ra}
+            header='The Winged Dragon of Ra'
+            description='Monster Card'
+            fluid color='brown'
+          />
+
+          <Card
+            as={Link} to ='/detail'
+            image={mirror_force}
+            header='Mirror Force'
+            description='Trap Card'
+            fluid color='purple'
+          />
+          <Card
+            as={Link} to ='/detail'
+            image={negate_attack}
+            header='Negate Attack'
+            description='Trap Card'
+            fluid color='purple'
+          />
+          <Card
+            as={Link} to ='/detail'
+            image={reverse_trap}
+            header='Reverse Trap'
+            description='Trap Card'
+            fluid color='purple'
+          />
+          <Card
+            as={Link} to ='/detail'
+            image={trap_hole}
+            header='Trap Hole'
+            description='Trap Card'
+            fluid color='purple'
+          />
+
+          <Card
+            as={Link} to ='/detail'
+            image={card_trader}
+            header='Card Trader'
+            description='Spell Card'
+            fluid color='green'
+          />
+          <Card
+            as={Link} to ='/detail'
+            image={dark_hole}
+            header='Dark Hole'
+            description='Spell Card'
+            fluid color='green'
+          />
+          <Card
+            as={Link} to ='/detail'
+            image={storm}
+            header='Storm'
+            description='Spell Card'
+            fluid color='green'
+          />
+          <Card
+            as={Link} to ='/detail'
+            image={swords_of_revealing_lights}
+            header='Swords of Revealing Light'
+            description='Spell Card'
+            fluid color='green'
+          />
+
+
+        </Card.Group>
+      
+      
+      
         
-        {/*<Card
-          image='http://cdn.pastemagazine.com/www/articles/assets_c/2019/06/TheOfficeQuote1-thumb-633x351-1115812.png'
-          header='Michael Scott'
-          description='this is michael'
-        />*/}
-
-        <Card
-          image={blueeyeswhitedragon}
-          header='Blue-Eyes White Dragon'
-          description='Monster Card'
-          fluid color='red'
-        />
-        <Card
-          image={dark_magician}
-          header='Dark Magician'
-          description='Monster Card'
-        />
-        <Card
-          image={exodia_the_forbidden_one}
-          header='Exodia the Forbidden One'
-          description='Monster Card'
-        />
-        <Card
-          image={the_winged_dragon_of_ra}
-          header='The Winged Dragon of Ra'
-          description='Monster Card'
-        />
-
-        <Card
-          image={mirror_force}
-          header='Mirror Force'
-          description='Trap Card'
-        />
-        <Card
-          image={negate_attack}
-          header='Negate Attack'
-          description='Trap Card'
-        />
-        <Card
-          image={reverse_trap}
-          header='Reverse Trap'
-          description='Trap Card'
-        />
-        <Card
-          image={trap_hole}
-          header='Trap Hole'
-          description='Trap Card'
-        />
-
-        <Card
-          image={card_trader}
-          header='Card Trader'
-          description='Spell Card'
-        />
-        <Card
-          image={dark_hole}
-          header='Dark Hole'
-          description='Spell Card'
-        />
-        <Card
-          image={storm}
-          header='Storm'
-          description='Spell Card'
-        />
-        <Card
-          image={swords_of_revealing_lights}
-          header='Swords of Revealing Light'
-          description='Spell Card'
-        />
-
-
-      </Card.Group>
-      
-      
-      <Card>
-        <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-        <Card.Content>
-          <Card.Header>Matthew</Card.Header>
-          <Card.Description>
-            Matthew is a musician living in Nashville.
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <a>
-            <Icon name='user' />
-            22 Friends
-          </a>
-        </Card.Content>
-      </Card>
- 
-
+      </div>
+      <Card className='mat'>
+          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Matthew</Card.Header>
+            <Card.Description>
+              Matthew is a musician living in Nashville.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              22 Friends
+            </a>
+          </Card.Content>
+        </Card>
     </div>
   );
 }
