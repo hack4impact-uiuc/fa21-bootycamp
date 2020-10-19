77e074dfa085a2ebcdda1c16e5a14347aaf3403c
Vasudha Chalasani <40867028+vasuchal@users.noreply.github.com>
2020-10-19 16:44:07 -0500

    modified backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+const cors = require('cors')
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
