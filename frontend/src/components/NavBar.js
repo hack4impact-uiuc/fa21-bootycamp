0a94cf842e503e2fb005d00e0a570a60856c3b7d
Vasu Chalasani <40867028+vasuchal@users.noreply.github.com>
2021-08-23 22:24:18 -0500

    Revert "Add redundant blocks"

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import { Menu } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+ import { Link } from "react-router-dom"; 
 
 export default function NavBar(props) {
   return (
     <div>
-      <Menu style={{ backgroundColor: "black", color: "white" }}>
-        <Menu.Item style={{ color: "white", fontSize: "20px", textShadow: "2px 2px black" }}><Link to="/">Home</Link></Menu.Item>
-        <Menu.Item style={{ color: "white", fontSize: "20px", textShadow: "2px 2px black" }}><Link to="/gallery">Gallery</Link></Menu.Item>
-        <Menu.Item style={{ color: "white", fontSize: "20px", textShadow: "2px 2px black" }}><Link to="/create">Create</Link></Menu.Item>
+      <Menu>
+        <Menu.Item><Link to="/">Home</Link></Menu.Item>
+        <Menu.Item><Link to="/gallery">Gallery</Link></Menu.Item>
+        <Menu.Item><Link to="/create">Create</Link></Menu.Item>
       </Menu>
     </div>
   );
