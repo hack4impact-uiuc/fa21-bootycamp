0cc22f4d532cff40339b10f76cf7aa0c5611dca1
vasuchal <vasudhachalasani@gmail.com>
2021-08-23 22:58:32 -0500

    Remove unused imports

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import { InfoBox } from "../components/InfoBox";
 
 import "../styles/LandingPage.css";
@@ -10,8 +9,6 @@ export default function LandingPage() {
       <InfoBox backColor="#64BEFD" heading="About This " paragraphDescription="What a cool website this is!! I can show you all the memes I love!" ></InfoBox>
       <InfoBox backColor="#00008B" heading="About The Creator " paragraphDescription="I like to meme!" leftChange="300px"></InfoBox>
       <InfoBox backColor="#64BEFD" heading="Iconic Memes and Vines" paragraphDescription="Meme! Meme! Meme!" leftChange="600px"></InfoBox>
-
-
     </div>
   );
 }
