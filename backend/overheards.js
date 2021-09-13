35448f98fc967c17a6464b00e1b589bacc974b8b
Matthew Walowski <mattwalowski@gmail.com>
2021-09-12 19:24:12 -0500

    Add overheards data

---
+++ backend/overheards.js
@@ -0,0 +1,27 @@
+const overheards = [
+  {name: "Yousef", quote: "I like fertilizing my members.", image: "https://uiuc.hack4impact.org/images/people/yousef_ahmed.jpg"},
+  {name: "Aditya", quote: "'wait, we’re a nonprofit?'   'I had a 10-page paper for a class and I just didn’t do it… now, the professor is emailing me worried' ", image: "https://uiuc.hack4impact.org/images/people/aditya_jain.jpg"} ,
+  {name: "Lam", quote: "i'd like to live in a city when I'm single, alone, and maybe dating", image: "https://uiuc.hack4impact.org/images/people/lam_tran.jpg"},
+  {name: "Angela", quote: "all the letters for 3some and 3dpme are really close togethe” :somekinkyshit:", image: "https://uiuc.hack4impact.org/images/people/angela_luo.jpg"},
+  {name: "Danielle", quote: "Danielle, 1 shot in: if Siraj was a girl he would be ma'am-raj", image: "https://uiuc.hack4impact.org/images/people/danielle_yang.jpg"},
+  {name: "Matthew", quote: "“corckodile” “crockodile” “crockadile” “crockidile” “crockadile” “crockidile”", image: "https://uiuc.hack4impact.org/images/people/matthew_walowski.jpg"} ,
+  {name: "Vasu", quote: "i think you mean daddy but i forgive you great aunt amit", image: "https://uiuc.hack4impact.org/images/people/vasu_chalasani.jpg"} ,
+  {name: "Kendall", quote: "Cool. So, so we've all been through death knights, particularly through the perspective of like a death", image: "https://uiuc.hack4impact.org/images/people/kendall_hester.jpg"} ,
+  {name: "Riya", quote: "people at uiuc really like this naru-naruto show... I mean hunter hunter", image: "https://uiuc.hack4impact.org/images/people/riya_jain.jpg"} ,
+  {name: "Albert", quote: "Danielle: You'd have to pay me to take your bathwater \
+  Albert: It's ok, I have a lot of buyers", image: "https://uiuc.hack4impact.org/images/people/albert_cao.jpg"},
+  {name: "Arman", quote: "i got laid when i was six… never again since", image: "https://uiuc.hack4impact.org/images/people/arman_rafati.jpg"},
+  {name: "Sabelle", quote:"'I wanna be a slim thicc hulk... let's say that's my goal' \
+   'oh nooo Im riding chancellor robert j jones'", image: "https://uiuc.hack4impact.org/images/people/sabelle_huang.jpg"},
+  {name: "Jeffrey", quote: "I’m a 12-inch guy", image: "https://uiuc.hack4impact.org/images/people/jeffrey_tang.jpg"} ,
+  {name: "Ayan", quote: "they don’t want us to go to the ISR dining hall ... but i’m a bad boy", image: "https://uiuc.hack4impact.org/images/people/ayan_mallik.jpg"} ,
+  {name: "Tiffany", quote: "“yeah, this reminds me of when I was in Singapore…” when talking about a book where the setting is in Idaho", image: "https://uiuc.hack4impact.org/images/people/tiffany_duong.jpg"} ,
+  {name: "Rebecca", quote: "boss makes a dollar, u make a dime, that’s why u vote for halloween candy on company time", image: "https://uiuc.hack4impact.org/images/people/rebecca_xun.jpg"} ,
+  {name: "Ashwin", quote: "Yousef: where’s the fungi section?“ Ashwin: -standing next to the wine and champagne- “this is the fun guy section!", image: "https://uiuc.hack4impact.org/images/people/ashwin_saxena.jpg"} ,
+  {name: "Sahi", quote: "When I signed up for CS, I thought I wouldn’t have to read", image: "https://uiuc.hack4impact.org/images/people/sahithi_muthyala.jpg"} ,
+  {name: "Ishaan", quote: "I want to get into stocks. I am bored at home and I want to feel the rush of investing and getting addicted to gambling", image: "https://uiuc.hack4impact.org/images/people/ishaan_sharma.jpg"} ,
+  {name: "Amit", quote: "“i think im ready for retirement”", image: "https://uiuc.hack4impact.org/images/people/amit_sawhney.jpg"} ,
+  {name: "Neha", quote: "*as soon as lam leaves* ”that feels good”", image: "https://uiuc.hack4impact.org/images/people/neha_konjeti.jpg"} ,
+  {name: "Nayonika", quote: "I’m excited for drug development and chemistry", image: "https://uiuc.hack4impact.org/images/people/nayonika_roy.jpg"} ,
+  {name: "Leo", quote: "elephants never forget", image: "https://uiuc.hack4impact.org/images/people/leonardo_galindo.jpg"}
+];
